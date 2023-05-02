c4305c833dfbb97ea92b3c550fd052c3417764ef
Daniel Čapla <Daniel.Capla1@ibm.com>
2023-05-02 21:36:56 +0200

    Add connection, Add Filereader to read properties dynamically

--- fizzbuzz/user-service/src/main/java/org/example/Database.java
+++ fizzbuzz/user-service/src/main/java/org/example/Database.java
@@ -1,5 +1,30 @@
 package org.example;
 
-public class Database {
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class Database {
+    private Connection connection;
+
+    public Database() {
+        Properties props = new Properties();
+        try (FileReader fr = new FileReader(".properties")) {
+            props.load(fr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(props.getProperty("url"), props.getProperty("username"), props.getProperty("password"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    public Database() {
+            connection = DriverManager.getConnection()
+        }
+    }
 }
