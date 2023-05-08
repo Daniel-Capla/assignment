6b919fcde0de040b86f7747028fefb3fb9f1c32d
Daniel Čapla <Daniel.Capla1@ibm.com>
2023-05-08 22:26:44 +0200

    Add test class, Add mockito dependency, Remove H2 dependency

---
+++ fizzbuzz/user-service/src/test/java/org/example/UserServiceImplTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+    @Mockito
+    private userDao userDao;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addUsers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteAll() {
+    }
+}
