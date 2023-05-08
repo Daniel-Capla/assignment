e9fd99f2b075289d622a8edbfecda7471b02f97c
Daniel Čapla <Daniel.Capla1@ibm.com>
2023-05-08 19:40:05 +0200

    Override run method in Consumer with busines logic

--- fizzbuzz/user-service/src/main/java/org/example/Consumer.java
+++ fizzbuzz/user-service/src/main/java/org/example/Consumer.java
@@ -1,15 +1,37 @@
 package org.example;
 
-import java.util.concurrent.BlockingDeque;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 
-public class Consumer {
-    private final BlockingDeque blockingDeque;
+public class Consumer implements Runnable{
+    private final BlockingQueue blockingQueue;
     private final UserDao userDao;
+    private final List<User> userList;
 
-    public Consumer(BlockingDeque blockingDeque, UserDao userDao) {
-        this.blockingDeque = blockingDeque;
+    public Consumer(BlockingQueue blockingQueue, UserDao userDao, List<User> userList) {
+        this.blockingQueue = blockingQueue;
         this.userDao = userDao;
+        this.userList = userList;
     }
 
-
+    @Override
+    public void run() {
+        try{
+            for (int i = 1; i <= userList.size(); i++) {
+                User user = (User) blockingQueue.take();
+                if (user.getUSER_ID() == -1) {
+                    break;
+                }
+                System.out.println("Consuming: " + user);
+                try{
+                    userDao.addUser(user);
+                } catch (SQLException e) {
+                    System.out.println("Could not add user on Dao: " + e.getMessage());
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- fizzbuzz/user-service/src/main/java/org/example/Producer.java
+++ fizzbuzz/user-service/src/main/java/org/example/Producer.java
@@ -1,13 +1,13 @@
 package org.example;
 
 import java.util.List;
-import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
 
 public class Producer {
-    private BlockingDeque<User> userQueue;
+    private BlockingQueue<User> userQueue;
 
 
-    public Producer(BlockingDeque<User> userQueue) {
+    public Producer(BlockingQueue<User> userQueue) {
         this.userQueue = userQueue;
     }
 
@@ -18,6 +18,8 @@ public class Producer {
                 System.out.println("Producing: " + user);
                 userQueue.put(user);
             }
+            //put predefined user with id -1 that will tell Consumer it's the end of the production
+            userQueue.put(new User(-1,"",""));
         } catch(Exception e){
             e.printStackTrace();
         }
