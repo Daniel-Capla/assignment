c85c8754fe146162d29d607b5ad11333d4aa8a4e
Daniel Čapla <Daniel.Capla1@ibm.com>
2023-05-08 18:40:01 +0200

    Add basic logic to producer class, start with Consumer class

--- fizzbuzz/user-service/src/main/java/org/example/Consumer.java
+++ fizzbuzz/user-service/src/main/java/org/example/Consumer.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.concurrent.BlockingDeque;
+
 public class Consumer {
+    private final BlockingDeque blockingDeque;
+    private final UserDao userDao;
+
+    public Consumer(BlockingDeque blockingDeque, UserDao userDao) {
+        this.blockingDeque = blockingDeque;
+        this.userDao = userDao;
+    }
+
+
 }
--- fizzbuzz/user-service/src/main/java/org/example/Producer.java
+++ fizzbuzz/user-service/src/main/java/org/example/Producer.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.util.List;
+import java.util.concurrent.BlockingDeque;
+
 public class Producer {
+    private BlockingDeque<User> userQueue;
+
+
+    public Producer(BlockingDeque<User> userQueue) {
+        this.userQueue = userQueue;
+    }
+
+    public void produceUsers(List<User> userList) {
+        try {
+            for (User user :
+                    userList) {
+                System.out.println("Producing: " + user);
+                userQueue.put(user);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
